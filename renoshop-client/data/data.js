019cda804fe2c0a32f63df006144558ddd46c76a
Andrii Shapoval <andrii.shapoval@smile-ukraine.com>
2021-04-10 22:11:11 +0300

    added json

---
+++ renoshop-client/data/data.js
@@ -0,0 +1,1 @@
+export const dataList = {"title": "Home Page", "text": "Lorem Ipsum is simply dummy text of the printi"};
--- renoshop-client/pages/index.js
+++ renoshop-client/pages/index.js
@@ -3,21 +3,58 @@ import styles from '../styles/Index.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '../components/layout'
+import {dataList} from '../data/data';
 
 
 
+// export async function getStaticProps() {
+//   // Get external data from the file system, API, DB, etc.
+
+//   // The value of the `props` key will be
+//   //  passed to the `Home` component
+//   return {
+//     props: {
+//       data: dataList
+//     }
+//   }
+// }
+
+export async function getServerSideProps(context) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/`)
+  const data = await res.json()
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {
+      data: data
+    }, // will be passed to the page component as props
+  }
+}
 
 
-export default function Index() {
+export default function Index({data}) {
+
+  const listItems = data.map((item) =>
+    <li>{item.title}</li>
+  );
+
   return (
-    <Layout home={true}>
+    <Layout home>
       <div className={styles.container}>
         <Head>
           <title>Index Page</title>
         </Head>
 
         <div className={styles.main}>
-          Index page
+        <ul>{listItems}</ul>
          
           <Link href="/shop-cart">
           <a>ShopCart</a>
@@ -28,3 +65,5 @@ export default function Index() {
     </Layout>
   )
 }
+
+
